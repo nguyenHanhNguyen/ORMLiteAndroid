dc7aa3fee2793a65cff88648857227a57d9587fb
nguyenHanhNguyen <nhn22801@gmail.com>
2015-05-08 21:02:43 +0700

    Delete done

--- app/src/main/java/com/example/nguyen/ormlitestudent/UpdateStudent.java
+++ app/src/main/java/com/example/nguyen/ormlitestudent/UpdateStudent.java
@@ -1,8 +1,11 @@
 package com.example.nguyen.ormlitestudent;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,9 +22,10 @@ public class UpdateStudent extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.update_student);
         Intent intent = getIntent();
-        int student_id = intent.getIntExtra(ShowAllStudents.STUDENT_ID,0);
+        final int student_id = intent.getIntExtra(ShowAllStudents.STUDENT_ID,0);
 
-        StudentDatabaseHelper db = new StudentDatabaseHelper(this);
+        final StudentDatabaseHelper db = new StudentDatabaseHelper(this);
+        final Context context = getApplicationContext();
         Dao<Student,Integer> studentDao = null;
 
         try {
@@ -47,5 +51,21 @@ public class UpdateStudent extends Activity {
         edittext_fname.setText(update_student.getFirstName());
         edittext_lname.setText(update_student.getLastName());
         edittext_address.setText(update_student.getAddress());
+
+        Button btn_delete = (Button) findViewById(R.id.btn_delete);
+        btn_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Dao<Student,Integer> delete_student = db.getDao();
+                    delete_student.deleteById(student_id);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                Intent all_student_intent = new Intent(context, ShowAllStudents.class);
+                startActivity(all_student_intent);
+
+            }
+        });
     }
 }
